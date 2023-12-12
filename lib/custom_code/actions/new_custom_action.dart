536c6ace0d98240f5536ca9e46397a46832d3a12
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-12 06:04:31 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/custom_code/actions/new_custom_action.dart
@@ -0,0 +1,13 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom actions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+Future newCustomAction() async {
+  // Add your function code here!
+  // THIS iS Sample of newCustomAction
+}
