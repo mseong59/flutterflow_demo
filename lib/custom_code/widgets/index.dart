ff7c14e006ca82c56dc9eb5151c6b086a4702cb1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-12 06:12:35 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/index.dart
+++ lib/custom_code/widgets/index.dart
@@ -1,1 +1,2 @@
 export 'custom_icon_calendar_widget.dart' show CustomIconCalendarWidget;
+export 'new_custom_widget2.dart' show NewCustomWidget2;
---
+++ lib/custom_code/widgets/new_custom_widget2.dart
@@ -0,0 +1,30 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom widgets
+import '/custom_code/actions/index.dart'; // Imports custom actions
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+class NewCustomWidget2 extends StatefulWidget {
+  const NewCustomWidget2({
+    Key? key,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  final double? width;
+  final double? height;
+
+  @override
+  _NewCustomWidget2State createState() => _NewCustomWidget2State();
+}
+
+class _NewCustomWidget2State extends State<NewCustomWidget2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
